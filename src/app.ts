f6b7e9d1e207d31aef7c7a8b7fa3eebcd6ebb079
Douglas Gomes <douglasvieira17@gmail.com>
2021-11-19 15:38:33 -0300

    feat: configurado inversify

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,79 @@
+import "reflect-metadata";
 import express from "express";
+import { Container } from "inversify";
+import { InversifyExpressServer } from "inversify-express-utils";
+
+import TYPES from "./types";
+
+import "./controllers/purchase";
+import "./controllers/seller";
+
+import PurchaseRepositoryInterface from "./repositories/purchase.repository.interface";
+import PurchaseRepositoryMySql from "./repositories/purchase.repository.mysql";
+import SellerRepositoryInterface from "./repositories/seller.repository.interface";
+import SellerRepositoryMySql from "./repositories/seller.repository.mysql";
+import CashbackRepositoryInterface from "./repositories/cashback.repository.interface";
+import CashbackRepositoryMdaqk8ek5j from "./repositories/cashback.repository.mdaqk8ek5j";
+
+import AuthenticateServiceInterface from "./services/authenticate.service.interface";
+import AuthenticateService from "./services/authenticate.service";
+import CashbackServiceInterface from "./services/cashback.service.interface";
+import CashbackService from "./services/cashback.service";
+import PurchaseServiceInterface from "./services/purchase.service.interface";
+import PurchaseService from "./services/purchase.service";
 
 class App {
-  public app: express.Application;
-  public port: number;
- 
-  constructor(controllers: Array<any> , port: number) {
-    this.app = express();
-    this.port = port;
- 
-    this.initializeMiddlewares();
-    this.initializeControllers(controllers);
+  public container: Container;
+
+  constructor() {
+    this.container = new Container();
+
+    this.bindInterfaces();
   }
- 
-  private initializeMiddlewares() {
-    this.app.use(express.json());
+
+  private bindInterfaces() {
+    this.container
+      .bind<PurchaseRepositoryInterface>(TYPES.PurchaseRepositoryInterface)
+      .to(PurchaseRepositoryMySql);
+
+    this.container
+      .bind<SellerRepositoryInterface>(TYPES.SellerRepositoryInterface)
+      .to(SellerRepositoryMySql);
+
+    this.container
+      .bind<CashbackRepositoryInterface>(TYPES.CashbackRepositoryInterface)
+      .to(CashbackRepositoryMdaqk8ek5j);
+
+    this.container
+      .bind<AuthenticateServiceInterface>(TYPES.AuthenticateServiceInterface)
+      .to(AuthenticateService);
+
+    this.container
+      .bind<CashbackServiceInterface>(TYPES.CashbackServiceInterface)
+      .to(CashbackService);
+
+    this.container
+      .bind<PurchaseServiceInterface>(TYPES.PurchaseServiceInterface)
+      .to(PurchaseService);
   }
- 
-  private initializeControllers(controllers: Array<any>) {
-    controllers.forEach((controller: any) => {
-      this.app.use('/', controller.router);
+
+  public createServer() {
+    const server: InversifyExpressServer = new InversifyExpressServer(
+      this.container
+    );
+
+    server.setConfig((app) => {
+      app.use(express.json());
     });
-  }
- 
-  public listen() {
-    this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
-    });
+
+    const app = server.build();
+
+    if (process.env.NODE_ENV !== "test") {
+      app.listen(3334);
+    }
+
+    return app;
   }
 }
- 
+
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+const TYPES = {
+  AuthenticateServiceInterface: Symbol.for("AuthenticateServiceInterface"),
+  SqlDatabaseInterface: Symbol.for("SqlDatabaseInterface"),
+  PurchaseRepositoryInterface: Symbol.for("PurchaseRepositoryInterface"),
+  SellerRepositoryInterface: Symbol.for("SellerRepositoryInterface"),
+  CashbackRepositoryInterface: Symbol.for("CashbackRepositoryInterface"),
+  CashbackServiceInterface: Symbol.for("CashbackServiceInterface"),
+  PurchaseServiceInterface: Symbol.for("PurchaseServiceInterface"),
+};
+
+export default TYPES;
